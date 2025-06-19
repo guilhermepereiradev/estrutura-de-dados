ac061c015f9cbdae22554794a4f9499b9583514d
guilhermepereiradev <guilhermepereira1609@hotmail.com>
2025-06-18 22:08:15 -0300

    exercicio passeio do cavalo

---
+++ passeio-do-cavalo/src/PasseioDoCavalo.java
@@ -0,0 +1,43 @@
+//public class PasseioDoCavalo {
+//    int n = 8;
+//    int i = 0, j = 0;
+//    int[][] t = new int[n][n];
+//    boolean q = false;
+//
+//    int[] s = {0, 1, 2, 3, 4, 5, 6, 7};
+//
+//    int[] h = {2, 1, -1, -2, -2, -1, 1, 2};
+//    int[] v = {1, 2, 2, 1, -1, -2, -2, -1};
+//
+//    while(i < n) {
+//        while(j < n) {
+//            t[i][j] = 0;
+//            j++;
+//        }
+//        i++;
+//    }
+//
+//    t[0][0] = 1;
+//
+//    tenta();
+//
+//        if (q)
+//            System.out.println("Tem solução");
+//        else
+//                System.out.println("Nâo tem solução");
+//
+//    void tenta(int i, int x, int y, boolean q, int n, int[]) {
+//        int m = 0;
+//        int xn;
+//        int yn;
+//        boolean q1 = false;
+//
+//        while (!q1 || m == n) {
+//            m = m++;
+//            q1 = false;
+//            xn = h[m];
+//        }
+//
+//        q = q1;
+//    }
+//}
